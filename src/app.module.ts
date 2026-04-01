b6bbd0e640f07c22d1ca9204f1eac0ab2a8886ce
Amani Franck <amanifranck2005@gmail.com>
2026-04-01 14:27:32 +0200

    adding the user folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { SupabaseService } from './supabase/supabase.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true, // so you can inject process.env anywhere
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [
--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -6,8 +6,8 @@ export class SupabaseService {
     public client: SupabaseClient;
 
     constructor() {
-        const supabaseUrl = process.env.SUPABASE_URL;
-        const supabaseKey = process.env.SUPABASE_ANON_KEY;
+        const supabaseUrl = process.env.SUPABASE_URL!;
+        const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
 
         if (!supabaseUrl || !supabaseKey) {
             throw new Error('SUPABASE_URL and SUPABASE_ANON_KEY must be set');
